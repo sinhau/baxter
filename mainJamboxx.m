d8b9b3ff62501ef58c2625f6d0f179623c57a6d8
Baxter <sinha.karsh@gmail.com>
2014-02-06 17:05:23 -0500

    cleanup

--- mainJamboxx.m
+++ mainJamboxx.m
@@ -13,16 +13,15 @@ jamboxx = RobotRaconteur.Connect('tcp://192.168.1.104:5318/{0}/Jamboxx');
 xbox = RobotRaconteur.Connect('tcp://192.168.1.104:5437/Xbox_controllerServer/xbox_controller');
 
 % Arduino connection
-o = RobotRaconteur.Connect('tcp://localhost:5001/{0}/Arduino');
+%o = RobotRaconteur.Connect('tcp://localhost:5001/{0}/Arduino');
 
 %% Main
 
 setBaxterConstants;
-stopProg = 0;
-kbhit('init');
 
 % Initialize left arm params
 linVel_L = [0;0;0];
+
 angVel_L = [0;0;0];
 wristVel_L = [];
 grip_L = [];
@@ -37,29 +36,25 @@ grip_R = [];
 % publisher_rightGripCal.publish([]);
 % pause(3);
 
-clc; disp('READY TO MOVE...PRESS Q TO QUIT');
+clc; disp('READY TO MOVE...');
 while(1) 
     
-    stopProg = kbhit;
-    if stopProg == 'Q'
-        close all;
-        break;
-    end
-    
-    % Arduino
-    xBoxInput = xbox.controller_input;
-    xInput = (xBoxInput.left_thumbstick_X)/130;
-    yInput = (xBoxInput.left_thumbstick_Y )/130;
-%     disp(xInput);
-%     disp(yInput);
-    o.arduino(int8(-1), int8(xInput));
-    o.arduino(int8(1), int8(yInput));
+%     % Arduino
+%     xBoxInput = xbox.controller_input;
+%     xInput = (xBoxInput.left_thumbstick_X)/130;
+%     yInput = (xBoxInput.left_thumbstick_Y )/130;
+% %     disp(xInput);
+% %     disp(yInput);
+%     o.arduino(int8(-1), int8(xInput));
+%     o.arduino(int8(1), int8(yInput));
  
     % Gather joint information
-    pause(0.01);
+    %pause(0.01);
     jointAngles = baxter.JointPositions;
     jointAnglesLeft = jointAngles(1:7);
     jointAnglesRight = jointAngles(8:14);
+    %baxter.setJointPosition('left',jointAnglesLeft); 
+    %baxter.setJointPosition('right',jointAnglesRight);
     
     % Set desired input using Jamboxx
     [linVel_L,linVel_R,wristVel_L,wristVel_R,grip_L,grip_R] = setDesiredInput(jamboxx);
@@ -103,26 +98,21 @@ while(1)
                 end
             end     
         else
+
             qDot_R = [0;0;0;0;0;0;0];
         end
 
     % Publish desired joint velocities
         % Left arm
-        leftJointVel = [qDot_L(1);qDot_L(2);...
-            qDot_L(3);qDot_L(4);qDot_L(5);...
-            qDot_L(6);qDot_L(7)];
         if ~isempty(wristVel_L)
-            leftJointVel(5:7) = wristVel_L;
+            qDot_L(5:7) = wristVel_L;
         end
-        baxter.setJointVelocity('left',leftJointVel);
+        baxter.setJointVelocity('left',qDot_L);
         %Right arm
-        rightJointVel = [qDot_R(1);qDot_R(2);...
-            qDot_R(3);qDot_R(4);qDot_R(5);...
-            qDot_R(6);qDot_R(7)];
         if ~isempty(wristVel_R)
-            rightJointVel(5:7) = wristVel_R;
+            qDot_R(5:7) = wristVel_R;
         end
-        baxter.setJointVelocity('right',rightJointVel);
+        baxter.setJointVelocity('right',qDot_R);
     
     % Publish grip position
         % Left gripper
